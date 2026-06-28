82396d1fe5e3e3811afa2e191812c6ea41d851a7
nikhil-kr-47 <kumar47nikhil@gmail.com>
2026-06-28 14:29:11 +0530

    Link fix

--- src/nav.jsx
+++ src/nav.jsx
@@ -2,7 +2,7 @@ export default function Navbar(){
     return(
         <div className="navbar">
           <h1>Demo Project</h1>
-          <a href="/user/add">Create new User</a>
+          <Link to="/user/add">Create new User</Link>
         </div>
     );
 }
